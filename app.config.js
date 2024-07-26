348ab138132c08caf64821a5f31a569ebfcbcb18
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2024-07-26 03:09:10 +0300

    add google-services.json as eas secret

---
+++ app.config.js
@@ -0,0 +1,45 @@
+export default {
+  expo: {
+    name: "marios-helper",
+    slug: "marios-helper",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.altany.marioshelper",
+      googleServicesFile: "./google-services.json",
+      //googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: ["expo-router"],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "96e3863b-2a0e-4ff1-9abc-83cb166fb511",
+      },
+    },
+  },
+};
